f287dd4d55a361a8bc46c20bd3b5d7b92d072530
Biplob Das <biplob90.d@gmail.com>
2024-11-28 12:26:09 +0200

    First Commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    description = "This is the bucket name of S3."
+  default = "mybucket-7865a"
+}
+
+variable "website_index_document" {
+  description = "This is the website index."
+  default = "index.html"
+}
